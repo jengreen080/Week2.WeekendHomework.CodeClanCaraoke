004accf575c909c32c6253002b203578705f77d0
Jennifer <jng-96@hotmail.co.uk>
2023-04-14 14:11:00 +0100

    starting to write add_guest_to_room test

--- src/room.py
+++ src/room.py
@@ -3,5 +3,14 @@ class Room:
     def __init__(self, name, capacity):
         self.room_name = name
         self.room_capacity = capacity
+        self.room_list_of_people_within_room = []
+        self.room_number_of_people_in_room = 0
+
+    def add_guest_to_room(self, guest):
+        self.room_people_within_room.append(guest)
+        self.room_number_of_people_in_room += 1
+
+
+
         
         
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,7 +1,7 @@
 import unittest
 from src.room import Room
 # from src.song import Song
-# from src.guest import Guest
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     
@@ -12,3 +12,7 @@ class TestRoom(unittest.TestCase):
 
     def test_check_room_name(self):
         self.assertEqual("Bowie", self.bowie_room.room_name) #passes
+
+    def test_add_guest_to_room(self):
+        self.assertEqual(1, self.add_guest_to_room(second_guest))
+        ### this is where we're at
