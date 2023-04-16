5ab949abe1fd4f88fdf1944771760700470a9638
Jennifer <jng-96@hotmail.co.uk>
2023-04-16 21:37:15 +0100

    as close to finished as I got

--- src/room.py
+++ src/room.py
@@ -3,7 +3,7 @@ class Room:
     def __init__(self, name, capacity):
         self.room_name = name
         self.room_capacity = capacity
-        # self.room_list_of_people_within_room = []
+        # self.room_list_of_people_within_room = {}
         self.room_list_of_people_within_room = []
         self.room_number_of_people_in_room = 0
         self.room_song_list = []
--- tests/room_test.py
+++ tests/room_test.py
@@ -22,33 +22,31 @@ class TestRoom(unittest.TestCase):
                                                 4 : [self.forth_guest.guest_name, self.forth_guest.guest_age],
                                                 5 : [self.fifth_guest.guest_name, self.fifth_guest.guest_age]}
 
-    # def test_check_room_name(self):
-    #     self.assertEqual("Bowie", self.bowie_room.room_name) #passes
-
-    # I find it hard to know what I need to define in setUp. If I've defined my guests in guest_test
+    def test_check_room_name(self):
+        self.assertEqual("Bowie", self.bowie_room.room_name) #passes
 
     def test_add_guest_to_room(self):
-        print(self.room_list_of_people_within_room)
-        print(self.bowie_room.room_number_of_people_in_room)
+        # print(self.room_list_of_people_within_room)
+        # print(self.bowie_room.room_number_of_people_in_room)
         number_of_people_in_room = self.bowie_room.add_guest_to_room(self.first_guest)
         number_of_people_in_room = self.bowie_room.add_guest_to_room(self.second_guest)
         self.assertEqual(2, number_of_people_in_room) 
-        print(self.bowie_room.room_list_of_people_within_room)
+        # print(self.bowie_room.room_list_of_people_within_room)
 
 
-    #### so confused here
+    #### How can i see if my code works when referring to an 'entire' guest, not just name/age etc.?
 
-    # def test_remove_guest_from_room(self):
-    #     self.room_list_of_people_within_room = [self.first_guest, self.second_guest, self.third_guest, self.forth_guest, self.fifth_guest]
-    #     print(self.room_list_of_people_within_room)
-    #     number_of_people_in_room = self.bowie_room.remove_guest_from_room(self.first_guest)
-    #     print(self.room_list_of_people_within_room)
-    #     self.assertEqual(4, number_of_people_in_room)
-    
-    # how woul I check to make sure the person is in the list?
-    # def check_name_of_person_in_room(self):
-    #     self.assertEqual(, self.bowie_room.Guest)
+    def test_remove_guest_from_room(self):
+        self.room_list_of_people_within_room = [self.first_guest.guest_name, self.second_guest.guest_name, self.third_guest.guest_name, self.forth_guest.guest_name, self.fifth_guest.guest_name]
+        # print(self.room_list_of_people_within_room)
+        number_of_people_in_room = self.bowie_room.remove_guest_from_room(self.first_guest.guest_name)
+        return number_of_people_in_room
+        self.assertEqual(4, number_of_people_in_room)
+        print(number_of_people_in_room)
+        print(self.room_list_of_people_within_room)
+        ## why has my code greyed itself out? Test seems to be running
 
-    # def test_add_song_to_room(self):
-    #     self.test_add_song_to_room(song1)
-    #     self.assertEqual(1, len(self.bowie_room.room_song_list))
+    def test_add_song_to_room(self):
+        self.test_add_song_to_room(self.song1.song_name)
+        ## why is the above line saying i'm giving in 2 positional arguemnts? I'm baffled. Not currently running
+        self.assertEqual(1, len(self.bowie_room.room_song_list))
